370eac50851d7e5107f10be11c16b814607aa8b4
yuminji <yuminji97@kakao.com>
2024-04-21 19:36:37 +0900

    update

--- dataframe.py
+++ dataframe.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -13,15 +13,15 @@ def plot_matplotlib(): # streamlit  문법
     st.title("Categorical Bar Plot with Seaborn") # 대시보드 제목
     df = load_data()
     fig, ax = plt.subplots() # 시각화
-
+    
     # Using Seaborn's barplot function
     sns.barplot(x=df['year'], y=df['lifeExp'], data=df, ax=ax)
-
+    
     # Labeling axes and title
     ax.set_xlabel("year")
     ax.set_ylabel("lifeExp")
     ax.set_title("Year vs. lifeExp")
-
+    
     st.pyplot(fig) # 대시보드에 출력
 
 def main():
@@ -31,11 +31,11 @@ def main():
     df = load_data()
     st.dataframe(df, use_container_width=True) # 대시보드에 표 삽입
 
-    #pandas style- 두번째 표
-    st.title("Maximum value per column")
+    #pandas style- 두번째 표 
+    st.title("Maximum value per column") 
     st.dataframe(df.iloc[:5,2:].style.highlight_max(axis=0))
 
-    plot_matplotlib() # 그래프 삽입
-
+    plot_matplotlib() # 그래프 삽입    
+    
 if __name__ == "__main__":
     main()
